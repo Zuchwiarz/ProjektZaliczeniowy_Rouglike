35d565a41dd379d686b9e174531204a0d9a4f50a
Zuchwiarz <w.burda@interia.pl>
2026-06-29 13:24:24 +0200

    stworzenie Vectora2 który przyjmuje int'y a nie tylko jakieś floaty

---
+++ Vector2.cs
@@ -0,0 +1,18 @@
+namespace GrTypuRouglike;
+
+public struct Vector2 // struct? czym jest struct?
+{// w sensie, chyba rozumiem czym jest, ale trzeba będzie później obczaić zasady któe nim rządzą
+
+    public int X;
+    public int Y;
+    
+    public Vector2(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    // PODSUMOWANIE
+    // > musimy stworzyć sztucznąfunkcję Vector 2
+    // bo orginalna przyjmuje tylko floaty, jaki mi się zdaje
+}
