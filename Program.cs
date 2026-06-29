728900c44c98141818a25db1a2b7245d2fc76271
Zuchwiarz <w.burda@interia.pl>
2026-06-29 13:38:23 +0200

    doszprycowanie klasy Program

--- Program.cs
+++ Program.cs
@@ -1,10 +1,11 @@
-﻿using System.Numerics;
+﻿using System.IO.Pipes;
+using System.Numerics;
 
 namespace GrTypuRouglike;
 
 public class Program
 {
-    static void Main()
+    public static void Main()
     {
         Console.CursorVisible = false;
         //STEROWANIE - przypisanie vektorów do klawiszy
@@ -17,7 +18,38 @@ public class Program
         //WCZYTYWANIE MAPY
             // 1. najpierw stworzymy CEll
             // 2. A potem Map()
-        //Map map = new Map();
-        //mapLoadFromFile("level1.txt");
+        Map map = new Map();
+        map.LoadFromFile("level1.txt");
+        
+        Item iten = new Item('☺', new Vector2(1, 2), map);
+        //^^ wydaje mi się że Program to nie jest dobre miejsce na rozrzucanie itemków po mapie
+        // !! dobrym będzie stworzyć jakąś funkcję w Map, albo dictionary MappItemToss
+        //który będzie przypisywał nazwęmapy, a potem grupę itemów które należy po niej losowo rozrzucić
+
+
+        bool isPlaying = true;
+        //POZYCJA STARTOWA - GRACZ
+        Vector2 startingPosition = new Vector2(4, 2);
+        Character hero = new Player('@', startingPosition, map, directions);
+        startingPosition.X = 1;
+        startingPosition.Y = 1;
+
+        Character anotherHero = new Npc('$', startingPosition, map);
+        List<Character> characters = [hero, anotherHero]; //<lista postaci na poziomie
+
+        map.Display(); //<< no tak, trza wyświetlić mapę xD
+
+        while (isPlaying)
+        {
+            foreach (Character character in characters)
+            {
+                isPlaying = character.TakeTurn(map);
+            }
+        }// hmm, takie proste, a takie trudne do zrozumienia to TakeTurn
+        
+        Console.WriteLine("Goodbye!"); //<<?????? a co to?
     }
+
+    
+
 }
